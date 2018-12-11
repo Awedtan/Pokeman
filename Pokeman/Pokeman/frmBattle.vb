127647533489f0c005e9af6fec6e240989c672ea
Awedtan <31903483+Awedtan@users.noreply.github.com>
2018-12-11 11:08:56 -0500

    Starting to implement battle system

---
+++ Pokeman/Pokeman/frmBattle.vb
@@ -0,0 +1,12 @@
+﻿Public Class frmBattle
+
+	Dim yourturn As Boolean = True
+
+	Dim youhealth As Integer = 
+
+	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+
+	End Sub
+End Class
