8e7d2bdaf36b1aeadcfc05bd71ddbe1070fcc46d
Awedtan <31903483+Awedtan@users.noreply.github.com>
2018-12-12 10:16:23 -0500

    Box stuff

--- Pokeman/Pokeman/frmBox.Designer.vb
+++ Pokeman/Pokeman/frmBox.Designer.vb
@@ -22,16 +22,422 @@ Partial Class frmBox
 	'Do not modify it using the code editor.
 	<System.Diagnostics.DebuggerStepThrough()> _
 	Private Sub InitializeComponent()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.Label3 = New System.Windows.Forms.Label()
+		Me.Label4 = New System.Windows.Forms.Label()
+		Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+		Me.Label5 = New System.Windows.Forms.Label()
+		Me.Label6 = New System.Windows.Forms.Label()
+		Me.Label7 = New System.Windows.Forms.Label()
+		Me.Label8 = New System.Windows.Forms.Label()
+		Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+		Me.Label9 = New System.Windows.Forms.Label()
+		Me.Label10 = New System.Windows.Forms.Label()
+		Me.Label11 = New System.Windows.Forms.Label()
+		Me.Label12 = New System.Windows.Forms.Label()
+		Me.PictureBox43 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox10 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox11 = New System.Windows.Forms.PictureBox()
+		Me.Button1 = New System.Windows.Forms.Button()
+		Me.Button2 = New System.Windows.Forms.Button()
+		Me.Button3 = New System.Windows.Forms.Button()
+		Me.Button4 = New System.Windows.Forms.Button()
+		Me.GroupBox1.SuspendLayout()
+		Me.GroupBox2.SuspendLayout()
+		Me.GroupBox3.SuspendLayout()
+		CType(Me.PictureBox43, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).BeginInit()
 		Me.SuspendLayout()
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Location = New System.Drawing.Point(6, 62)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(46, 13)
+		Me.Label1.TabIndex = 0
+		Me.Label1.Text = "Pikachu"
+		'
+		'GroupBox1
+		'
+		Me.GroupBox1.Controls.Add(Me.Button4)
+		Me.GroupBox1.Controls.Add(Me.Button3)
+		Me.GroupBox1.Controls.Add(Me.Button2)
+		Me.GroupBox1.Controls.Add(Me.Button1)
+		Me.GroupBox1.Controls.Add(Me.PictureBox3)
+		Me.GroupBox1.Controls.Add(Me.PictureBox2)
+		Me.GroupBox1.Controls.Add(Me.PictureBox1)
+		Me.GroupBox1.Controls.Add(Me.PictureBox43)
+		Me.GroupBox1.Controls.Add(Me.Label4)
+		Me.GroupBox1.Controls.Add(Me.Label3)
+		Me.GroupBox1.Controls.Add(Me.Label2)
+		Me.GroupBox1.Controls.Add(Me.Label1)
+		Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+		Me.GroupBox1.Name = "GroupBox1"
+		Me.GroupBox1.Size = New System.Drawing.Size(200, 359)
+		Me.GroupBox1.TabIndex = 1
+		Me.GroupBox1.TabStop = False
+		Me.GroupBox1.Text = "GroupBox1"
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.Location = New System.Drawing.Point(6, 139)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(64, 13)
+		Me.Label2.TabIndex = 6
+		Me.Label2.Text = "Charmander"
+		'
+		'Label3
+		'
+		Me.Label3.AutoSize = True
+		Me.Label3.Location = New System.Drawing.Point(6, 216)
+		Me.Label3.Name = "Label3"
+		Me.Label3.Size = New System.Drawing.Size(42, 13)
+		Me.Label3.TabIndex = 8
+		Me.Label3.Text = "Squirtle"
+		'
+		'Label4
+		'
+		Me.Label4.AutoSize = True
+		Me.Label4.Location = New System.Drawing.Point(6, 293)
+		Me.Label4.Name = "Label4"
+		Me.Label4.Size = New System.Drawing.Size(54, 13)
+		Me.Label4.TabIndex = 10
+		Me.Label4.Text = "Bulbasaur"
+		'
+		'GroupBox2
+		'
+		Me.GroupBox2.Controls.Add(Me.PictureBox7)
+		Me.GroupBox2.Controls.Add(Me.PictureBox6)
+		Me.GroupBox2.Controls.Add(Me.PictureBox5)
+		Me.GroupBox2.Controls.Add(Me.PictureBox4)
+		Me.GroupBox2.Controls.Add(Me.Label5)
+		Me.GroupBox2.Controls.Add(Me.Label6)
+		Me.GroupBox2.Controls.Add(Me.Label7)
+		Me.GroupBox2.Controls.Add(Me.Label8)
+		Me.GroupBox2.Location = New System.Drawing.Point(218, 15)
+		Me.GroupBox2.Name = "GroupBox2"
+		Me.GroupBox2.Size = New System.Drawing.Size(200, 359)
+		Me.GroupBox2.TabIndex = 11
+		Me.GroupBox2.TabStop = False
+		Me.GroupBox2.Text = "GroupBox2"
+		'
+		'Label5
+		'
+		Me.Label5.AutoSize = True
+		Me.Label5.Location = New System.Drawing.Point(6, 293)
+		Me.Label5.Name = "Label5"
+		Me.Label5.Size = New System.Drawing.Size(46, 13)
+		Me.Label5.TabIndex = 10
+		Me.Label5.Text = "Pikachu"
+		'
+		'Label6
+		'
+		Me.Label6.AutoSize = True
+		Me.Label6.Location = New System.Drawing.Point(6, 216)
+		Me.Label6.Name = "Label6"
+		Me.Label6.Size = New System.Drawing.Size(46, 13)
+		Me.Label6.TabIndex = 8
+		Me.Label6.Text = "Pikachu"
+		'
+		'Label7
+		'
+		Me.Label7.AutoSize = True
+		Me.Label7.Location = New System.Drawing.Point(6, 139)
+		Me.Label7.Name = "Label7"
+		Me.Label7.Size = New System.Drawing.Size(64, 13)
+		Me.Label7.TabIndex = 6
+		Me.Label7.Text = "Charmander"
+		'
+		'Label8
+		'
+		Me.Label8.AutoSize = True
+		Me.Label8.Location = New System.Drawing.Point(6, 62)
+		Me.Label8.Name = "Label8"
+		Me.Label8.Size = New System.Drawing.Size(46, 13)
+		Me.Label8.TabIndex = 0
+		Me.Label8.Text = "Pikachu"
+		'
+		'GroupBox3
+		'
+		Me.GroupBox3.Controls.Add(Me.PictureBox11)
+		Me.GroupBox3.Controls.Add(Me.PictureBox10)
+		Me.GroupBox3.Controls.Add(Me.PictureBox9)
+		Me.GroupBox3.Controls.Add(Me.PictureBox8)
+		Me.GroupBox3.Controls.Add(Me.Label9)
+		Me.GroupBox3.Controls.Add(Me.Label10)
+		Me.GroupBox3.Controls.Add(Me.Label11)
+		Me.GroupBox3.Controls.Add(Me.Label12)
+		Me.GroupBox3.Location = New System.Drawing.Point(424, 15)
+		Me.GroupBox3.Name = "GroupBox3"
+		Me.GroupBox3.Size = New System.Drawing.Size(200, 359)
+		Me.GroupBox3.TabIndex = 12
+		Me.GroupBox3.TabStop = False
+		Me.GroupBox3.Text = "GroupBox3"
+		'
+		'Label9
+		'
+		Me.Label9.AutoSize = True
+		Me.Label9.Location = New System.Drawing.Point(6, 293)
+		Me.Label9.Name = "Label9"
+		Me.Label9.Size = New System.Drawing.Size(46, 13)
+		Me.Label9.TabIndex = 10
+		Me.Label9.Text = "Pikachu"
+		'
+		'Label10
+		'
+		Me.Label10.AutoSize = True
+		Me.Label10.Location = New System.Drawing.Point(6, 216)
+		Me.Label10.Name = "Label10"
+		Me.Label10.Size = New System.Drawing.Size(46, 13)
+		Me.Label10.TabIndex = 8
+		Me.Label10.Text = "Pikachu"
+		'
+		'Label11
+		'
+		Me.Label11.AutoSize = True
+		Me.Label11.Location = New System.Drawing.Point(6, 139)
+		Me.Label11.Name = "Label11"
+		Me.Label11.Size = New System.Drawing.Size(64, 13)
+		Me.Label11.TabIndex = 6
+		Me.Label11.Text = "Charmander"
+		'
+		'Label12
+		'
+		Me.Label12.AutoSize = True
+		Me.Label12.Location = New System.Drawing.Point(6, 62)
+		Me.Label12.Name = "Label12"
+		Me.Label12.Size = New System.Drawing.Size(46, 13)
+		Me.Label12.TabIndex = 0
+		Me.Label12.Text = "Pikachu"
+		'
+		'PictureBox43
+		'
+		Me.PictureBox43.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.PictureBox43.Location = New System.Drawing.Point(119, 31)
+		Me.PictureBox43.Name = "PictureBox43"
+		Me.PictureBox43.Size = New System.Drawing.Size(75, 71)
+		Me.PictureBox43.TabIndex = 46
+		Me.PictureBox43.TabStop = False
+		'
+		'PictureBox1
+		'
+		Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.PictureBox1.Location = New System.Drawing.Point(119, 108)
+		Me.PictureBox1.Name = "PictureBox1"
+		Me.PictureBox1.Size = New System.Drawing.Size(75, 71)
+		Me.PictureBox1.TabIndex = 47
+		Me.PictureBox1.TabStop = False
+		'
+		'PictureBox2
+		'
+		Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.PictureBox2.Location = New System.Drawing.Point(119, 185)
+		Me.PictureBox2.Name = "PictureBox2"
+		Me.PictureBox2.Size = New System.Drawing.Size(75, 71)
+		Me.PictureBox2.TabIndex = 48
+		Me.PictureBox2.TabStop = False
+		'
+		'PictureBox3
+		'
+		Me.PictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.PictureBox3.Location = New System.Drawing.Point(119, 262)
+		Me.PictureBox3.Name = "PictureBox3"
+		Me.PictureBox3.Size = New System.Drawing.Size(75, 71)
+		Me.PictureBox3.TabIndex = 49
+		Me.PictureBox3.TabStop = False
+		'
+		'PictureBox4
+		'
+		Me.PictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.PictureBox4.Location = New System.Drawing.Point(119, 28)
+		Me.PictureBox4.Name = "PictureBox4"
+		Me.PictureBox4.Size = New System.Drawing.Size(75, 71)
+		Me.PictureBox4.TabIndex = 50
+		Me.PictureBox4.TabStop = False
+		'
+		'PictureBox5
+		'
+		Me.PictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.PictureBox5.Location = New System.Drawing.Point(119, 105)
+		Me.PictureBox5.Name = "PictureBox5"
+		Me.PictureBox5.Size = New System.Drawing.Size(75, 71)
+		Me.PictureBox5.TabIndex = 51
+		Me.PictureBox5.TabStop = False
+		'
+		'PictureBox6
+		'
+		Me.PictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.PictureBox6.Location = New System.Drawing.Point(119, 182)
+		Me.PictureBox6.Name = "PictureBox6"
+		Me.PictureBox6.Size = New System.Drawing.Size(75, 71)
+		Me.PictureBox6.TabIndex = 52
+		Me.PictureBox6.TabStop = False
+		'
+		'PictureBox7
+		'
+		Me.PictureBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.PictureBox7.Location = New System.Drawing.Point(119, 259)
+		Me.PictureBox7.Name = "PictureBox7"
+		Me.PictureBox7.Size = New System.Drawing.Size(75, 71)
+		Me.PictureBox7.TabIndex = 53
+		Me.PictureBox7.TabStop = False
+		'
+		'PictureBox8
+		'
+		Me.PictureBox8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.PictureBox8.Location = New System.Drawing.Point(119, 259)
+		Me.PictureBox8.Name = "PictureBox8"
+		Me.PictureBox8.Size = New System.Drawing.Size(75, 71)
+		Me.PictureBox8.TabIndex = 54
+		Me.PictureBox8.TabStop = False
+		'
+		'PictureBox9
+		'
+		Me.PictureBox9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.PictureBox9.Location = New System.Drawing.Point(119, 182)
+		Me.PictureBox9.Name = "PictureBox9"
+		Me.PictureBox9.Size = New System.Drawing.Size(75, 71)
+		Me.PictureBox9.TabIndex = 55
+		Me.PictureBox9.TabStop = False
+		'
+		'PictureBox10
+		'
+		Me.PictureBox10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.PictureBox10.Location = New System.Drawing.Point(119, 105)
+		Me.PictureBox10.Name = "PictureBox10"
+		Me.PictureBox10.Size = New System.Drawing.Size(75, 71)
+		Me.PictureBox10.TabIndex = 56
+		Me.PictureBox10.TabStop = False
+		'
+		'PictureBox11
+		'
+		Me.PictureBox11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.PictureBox11.Location = New System.Drawing.Point(119, 28)
+		Me.PictureBox11.Name = "PictureBox11"
+		Me.PictureBox11.Size = New System.Drawing.Size(75, 71)
+		Me.PictureBox11.TabIndex = 57
+		Me.PictureBox11.TabStop = False
+		'
+		'Button1
+		'
+		Me.Button1.Location = New System.Drawing.Point(1, 78)
+		Me.Button1.Name = "Button1"
+		Me.Button1.Size = New System.Drawing.Size(59, 24)
+		Me.Button1.TabIndex = 50
+		Me.Button1.Text = "Button1"
+		Me.Button1.UseVisualStyleBackColor = True
+		'
+		'Button2
+		'
+		Me.Button2.Location = New System.Drawing.Point(1, 155)
+		Me.Button2.Name = "Button2"
+		Me.Button2.Size = New System.Drawing.Size(59, 24)
+		Me.Button2.TabIndex = 51
+		Me.Button2.Text = "Button2"
+		Me.Button2.UseVisualStyleBackColor = True
+		'
+		'Button3
+		'
+		Me.Button3.Location = New System.Drawing.Point(1, 232)
+		Me.Button3.Name = "Button3"
+		Me.Button3.Size = New System.Drawing.Size(59, 24)
+		Me.Button3.TabIndex = 52
+		Me.Button3.Text = "Button3"
+		Me.Button3.UseVisualStyleBackColor = True
+		'
+		'Button4
+		'
+		Me.Button4.Location = New System.Drawing.Point(1, 309)
+		Me.Button4.Name = "Button4"
+		Me.Button4.Size = New System.Drawing.Size(59, 24)
+		Me.Button4.TabIndex = 53
+		Me.Button4.Text = "Button4"
+		Me.Button4.UseVisualStyleBackColor = True
 		'
 		'frmBox
 		'
 		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.ClientSize = New System.Drawing.Size(613, 279)
+		Me.ClientSize = New System.Drawing.Size(661, 386)
+		Me.Controls.Add(Me.GroupBox3)
+		Me.Controls.Add(Me.GroupBox2)
+		Me.Controls.Add(Me.GroupBox1)
 		Me.Name = "frmBox"
 		Me.Text = "frmBox"
+		Me.GroupBox1.ResumeLayout(False)
+		Me.GroupBox1.PerformLayout()
+		Me.GroupBox2.ResumeLayout(False)
+		Me.GroupBox2.PerformLayout()
+		Me.GroupBox3.ResumeLayout(False)
+		Me.GroupBox3.PerformLayout()
+		CType(Me.PictureBox43, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).EndInit()
 		Me.ResumeLayout(False)
 
 	End Sub
+
+	Friend WithEvents Label1 As Label
+	Friend WithEvents GroupBox1 As GroupBox
+	Friend WithEvents Label2 As Label
+	Friend WithEvents Label4 As Label
+	Friend WithEvents Label3 As Label
+	Friend WithEvents GroupBox2 As GroupBox
+	Friend WithEvents Label5 As Label
+	Friend WithEvents Label6 As Label
+	Friend WithEvents Label7 As Label
+	Friend WithEvents Label8 As Label
+	Friend WithEvents GroupBox3 As GroupBox
+	Friend WithEvents Label9 As Label
+	Friend WithEvents Label10 As Label
+	Friend WithEvents Label11 As Label
+	Friend WithEvents Label12 As Label
+	Friend WithEvents Button4 As Button
+	Friend WithEvents Button3 As Button
+	Friend WithEvents Button2 As Button
+	Friend WithEvents Button1 As Button
+	Friend WithEvents PictureBox3 As PictureBox
+	Friend WithEvents PictureBox2 As PictureBox
+	Friend WithEvents PictureBox1 As PictureBox
+	Friend WithEvents PictureBox43 As PictureBox
+	Friend WithEvents PictureBox7 As PictureBox
+	Friend WithEvents PictureBox6 As PictureBox
+	Friend WithEvents PictureBox5 As PictureBox
+	Friend WithEvents PictureBox4 As PictureBox
+	Friend WithEvents PictureBox11 As PictureBox
+	Friend WithEvents PictureBox10 As PictureBox
+	Friend WithEvents PictureBox9 As PictureBox
+	Friend WithEvents PictureBox8 As PictureBox
 End Class
