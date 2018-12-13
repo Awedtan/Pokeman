f25d5d5c6cd53def310fdd013886d5fcec55c46e
Awedtan <31903483+Awedtan@users.noreply.github.com>
2018-12-13 09:47:59 -0500

    Add files via upload

---
+++ Pokeman/Pokeman/frmBox.vb
@@ -0,0 +1,13 @@
+﻿Public Class frmBox
+
+	Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+		If frmMainMenu.Bulbasaur(4) = 1 Then
+
+			Button1.Enabled = True
+
+		End If
+
+	End Sub
+
+End Class
